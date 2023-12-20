9aebf59b8d2175dde847b308c3f5096c867dfc5a
Oakar <oakkarrr00@gmail.com>
2023-12-20 12:28:07 +0800

    added photos to education

--- src/pages/About/Education.js
+++ src/pages/About/Education.js
@@ -16,8 +16,8 @@ function Education(data) {
 	});
 	return (
 		<div>
-			<div className="flex flex-col md:flex-row my-4 py-4 border-gray-500 border-2 rounded-lg">
-				<div className="px-6 py-2 w-full  items-center text-start ">
+			<div className="flex flex-col-reverse md:flex-row my-4 pt-4 border-gray-500 border-2 rounded-lg">
+				<div className="px-6 pt-2 w-full  items-start text-start ">
 					<h2 className="flex text-start text-2xl">{title}</h2>
 					<h2 className="flex text-lg italic pb-2">
 						{major}, {year}
@@ -25,10 +25,10 @@ function Education(data) {
 					<h2 className="flex text-md font-bold">Courses:</h2>
 					<p className="flex text-sm">{course}</p>
 				</div>
-				{/* <div className="flex px-6 py-2 items-center justify-center">
-					{score ? <iframe src={score} /> : <></>}
+				<div className="flex px-6 py-2 items-center justify-center">
+					{/* {score ? <iframe src={score} /> : <></>} */}
 					{image ? <img className="object-contain h-48" src={image} /> : <></>}
-				</div> */}
+				</div>
 			</div>
 		</div>
 	);
